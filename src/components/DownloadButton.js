39097e1ceb14d48072ce3a0ba432e9f4ec5dbbc0
Daniel <ddge07@gmail.com>
2024-04-08 09:21:15 -0500

    update button

--- src/components/DownloadButton.js
+++ src/components/DownloadButton.js
@@ -3,7 +3,7 @@ import React from 'react';
 const DownloadButton = () => {
   const handleDownload = () => {
     // Cambia 'nombre_archivo.pdf' por el nombre de tu archivo PDF en el repositorio
-    const pdfUrl = 'https://tuusuario.github.io/ruta/al/archivo/nombre_archivo.pdf';
+    const pdfUrl = 'https://raw.githubusercontent.com/DanielDGe/danieldge.github.io/main/src/assets/cv.pdf';
     const link = document.createElement('a');
     link.href = pdfUrl;
     link.setAttribute('download', 'mi_curriculum.pdf');
